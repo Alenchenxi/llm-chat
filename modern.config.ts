696662e69f0addda22aee80c2bcd6c405b39bcb7
alen <2423512757@qq.com>
2024-11-15 22:21:44 +0800

    feat: 启用Reduck状态管理

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,14 @@
-import { appTools, defineConfig } from '@modern-js/app-tools';
+import { appTools, defineConfig } from "@modern-js/app-tools";
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   runtime: {
     router: true,
+    state: true, // 启用状态管理
   },
   plugins: [
     appTools({
-      bundler: 'rspack', // Set to 'webpack' to enable webpack
+      bundler: "rspack", // Set to 'webpack' to enable webpack
     }),
   ],
 });
